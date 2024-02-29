9f140d5bb1152f36a71ea06e4f034ceb4bb06c80
joshuakirsch <69362392+joshuakirsch@users.noreply.github.com>
2024-02-29 10:03:12 -0500

    Update pseudoR_multi-reference.sh
    Added overwrite=true to both of the quality control steps

--- pseudoR_multi-reference.sh
+++ pseudoR_multi-reference.sh
@@ -54,11 +54,11 @@ do
 bbduk.sh in1=../${dedupe}/${i}${read1_ending} \
 in2=../${dedupe}/${i}${read2_ending} \
 out=temp/read1.fq out2=temp/read2.fq \
-qtrim=rl threads=${max_threads}
+qtrim=rl threads=${max_threads} overwrite=true
 
 bbduk.sh in=../${dedupe}/${i}${singleton_ending} \
 out=temp/readS.fq \
-qtrim=rl threads=${max_threads}
+qtrim=rl threads=${max_threads} overwrite=true
 
 #build contigs mapping database
 seqkit_var="${i}""${contig_ending}"
