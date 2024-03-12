8898293d3907d365a7bab230cad970ca693cd6c2
joshuakirsch <69362392+joshuakirsch@users.noreply.github.com>
2024-03-12 14:23:04 -0500

    Update Final_Output.multiReference.R
    Removed no_IRs and ISOSDB_names issues

--- Final_Output.multiReference.R
+++ Final_Output.multiReference.R
@@ -45,21 +45,14 @@ analysis_annot = analysis_annot %>%
   rename ("3prime_IS_depth" = `3prime`)
 
 #this block removes IS elements false hits (likely) and adds a better naming strategy for the IS elements
-no_IRs_ID = read_tsv(paste (args[3],"/no_IRs.ID.tsv",sep=""), col_names = "IS")
-
-analysis_annot = analysis_annot %>% rowwise() %>%
-  filter (! (IS %in% no_IRs_ID$IS))
 
 IS_fam_df  = read_tsv(paste (args[3],"/IS_fam_annot.txt", sep="")) %>%
   rowwise () %>%
   mutate (IS = gsub("_", ":", IS))
-IS_names  = read_tsv (paste (args[3],"/ISOSDB_names.tsv", sep =""), col_names = c("IS", "ISOSDB_name")) %>%
-  rowwise () %>%
-  mutate (IS = gsub("_", ":", IS))
+
 
 analysis_annot = analysis_annot %>% 
-  left_join(IS_fam_df) %>%
-  left_join(IS_names)
+  left_join(IS_fam_df)
 
 if (args[4] == "contig"){
   inOrf  = read_tsv ("final_results/IS_hits_in_orfs.txt", 
@@ -72,16 +65,12 @@ if (args[4] == "contig"){
     mutate (intragenic =ifelse(is.na(intragenic), "No", intragenic)) %>%
     distinct() %>%
     rename ("Insertion_Position" = max_depth_site_insertion_pos) %>%
-    rename ("IS_Depth_at_Max_Depth_Site" = max_depth_site_depth) %>%
-    rename ("IS_element" = ISOSDB_name) %>%
-    select (!IS)
+    rename ("IS_Depth_at_Max_Depth_Site" = max_depth_site_depth) 
   write_tsv (analysis_annot, "final_results/pseudoR_output.contig.tsv")
 } else if (args[4] == "ORF"){
   analysis_annot = analysis_annot %>%
     mutate ("ORF"=contig) %>%
     rename ("Insertion_Position" = max_depth_site_insertion_pos) %>%
-    rename ("IS_Depth_at_Max_Depth_Site" = max_depth_site_depth)%>%
-    rename ("IS_element" = ISOSDB_name) %>%
-    select (!IS)
+    rename ("IS_Depth_at_Max_Depth_Site" = max_depth_site_depth)
   write_tsv (analysis_annot, "final_results/pseudoR_output.ORF.tsv")
 }
